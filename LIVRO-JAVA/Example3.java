e2587744250e2f3350d779ae106c9aaa6b418cbe
Ygor Nascimento <ygor.nascimento@outlook.com>
2024-01-21 10:35:51 -0300

    End of Example 3

---
+++ LIVRO-JAVA/Example3.java
@@ -0,0 +1,18 @@
+class Example3 {
+    public static void main(String args[]) {
+        int var;
+        double x;
+
+        var = 10;
+        x = 10.0;
+
+        System.out.println("Original value of var: " + var);
+        System.out.println("Original Value of x: " + x);
+
+        var = var / 4;
+        x = x / 4;
+
+        System.out.println("var after division: " +  var);
+        System.out.println("x after division: " +  x);
+    }
+}
