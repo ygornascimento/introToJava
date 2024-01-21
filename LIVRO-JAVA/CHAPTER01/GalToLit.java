381ca3c361a18efe3a3cc382d9e8ce4ec23b9f37
Ygor Nascimento <ygor.nascimento@outlook.com>
2024-01-21 13:42:13 -0300

    End of Chapter 1

---
+++ LIVRO-JAVA/CHAPTER01/GalToLit.java
@@ -0,0 +1,11 @@
+class GalTolit {
+    public static void main(String arg[]) {
+        double gallons;
+        double liters;
+
+        gallons = 10;
+        liters  = gallons * 3.7854;
+
+        System.out.println(gallons + " gallons is " + liters + " liters.");
+    }
+}
